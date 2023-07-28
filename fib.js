1ed96d29c127900f7add41e51483ad48245347fe
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-07-27 22:35:45 -0500

    add iterative version of fib function

---
+++ fib.js
@@ -0,0 +1,24 @@
+// function fibs(n) {
+//     let fibArray = [];
+//     let numA = 0;
+//     let numB = 1;
+//     let numC = numA + numB;
+
+//     for (let i = 0; i < n - 3; i++) {
+//         if (numA === 0) {
+//             fibArray.push(numA);
+//             fibArray.push(numB);
+//             fibArray.push(numC);
+//         }
+        
+//         numA = numB;
+//         numB = numC;
+//         numC = numA + numB;
+//         fibArray.push(numC);
+//     }
+
+//     return fibArray;
+// }
+
+// fibs recursive
+
